5988975897e3dd6fd79d70904e369920c8a0b99c
Mirko Fañez <mirko-kert@hotmail.com>
2016-02-06 20:49:47 +0100

    Mejoras en la interfaz y obtención de más datos
    Añadida la interfaz, splash screen, los nombres de las paradas, nombre
    del bus, lista de buses disponibles y otras mejoras de aspecto.
    
    -Mirko-
    www.mirkoo.es

--- Buses.cs
+++ Buses.cs
@@ -11,41 +11,174 @@ namespace Prueba_XMLOpenData
 {
     class Buses
     {
-        string pathOpenData = "http://datos.gijon.es/doc/transporte/busgijontr.xml";
+        const string pathOpenData = "http://datos.gijon.es/doc/transporte/busgijontr.xml";
         XNamespace openDataNameSpace = "http://docs.gijon.es/sw/busgijon.asmx";
+        const string pathBusInfo = "http://datos.gijon.es/doc/transporte/busgijoninfo.xml"; // XML con los nombres de paradas
+
+        List<string> idsParadas;
+        List<int> minsParadas;
+        Dictionary<string,string> nombresParadas; // Diccionario con idParada, nombre
 
         /**Realiza una consulta con un número de bus especificado*/
         public void query(string busNumber)
         {
             try
             {
+                // Inicializamos las listas
+                idsParadas = new List<string>();
+                minsParadas = new List<int>();
+                nombresParadas = new Dictionary<string, string>();
+
                 XDocument document = XDocument.Load(pathOpenData);
                 XElement root = document.Root;
 
                 //Guarda toda la información de los buses
-                IEnumerable<XElement> busses = root.Element("llegadas").Elements(openDataNameSpace + "llegada");
+                IEnumerable<XElement> buses = root.Element("llegadas").Elements(openDataNameSpace + "llegada");
 
-                //Se realiza una consulta buscando el bus correspondiente
-                var query = from XElement element in busses
+                // Se realiza una consulta buscando el bus correspondiente.
+                // Ordenado por los minutos que le faltan
+                var query = from XElement element in buses
                             where element.Element(openDataNameSpace + "idlinea").Value.ToString() == busNumber
+                            orderby Convert.ToInt32(element.Element(openDataNameSpace + "minutos").Value)
                             select element;
 
-                //Si se encontraron correspondencais con el número de bus introducido se muestran
-                if ((query as IEnumerable<XElement>).Count() > 0)
+                //Si se encontraron correspondencias con el número de bus introducido se muestran
+                if (query.Count() > 0)
                 {
+                    // Mostramos una cabecera con el nombre del bus
+                    cabeceraInfo(busNumber);
+                    
                     foreach (XElement e in query)
-                        Console.WriteLine("Parada: " + e.Element(openDataNameSpace + "idparada").Value +
-                            "\tTiempo: " + e.Element(openDataNameSpace + "minutos").Value + "\n");
+                    {
+                        // Obtenemos los datos
+                        string idParada = e.Element(openDataNameSpace + "idparada").Value;
+                        int minutos = Convert.ToInt32(e.Element(openDataNameSpace + "minutos").Value);
+
+                        // Los añadimos a las listas
+                        idsParadas.Add(idParada);
+                        minsParadas.Add(minutos);
+                    }
+
+                    // Obtenemos los nombres de todas las paradas encontradas
+                    obtenerNombresParadas(idsParadas);
+
+                    // Lo mostramos todo
+                    Console.WriteLine(String.Format(" {0,-7}  {1}", "MINUTOS", "PARADA"));
+                    Console.WriteLine("----------------------------------------");
+
+                    for(int i = 0; i < idsParadas.Count; i++)
+                    {
+                        // Obtenemos los datos de las paradas
+                        string id = idsParadas[i];
+                        string nombre;
+                        nombresParadas.TryGetValue(id, out nombre);
+
+                        if (nombre != null)
+                        {
+                            Console.WriteLine(String.Format(" {0,-7}  {1} ({2})",
+                                minsParadas[i].ToString("00"), nombre, id));
+                        }
+                    }
                 }
                 else
-                    Console.WriteLine("No se encontraron correspondencias con el bus introducido.");
+                    Console.WriteLine("\nNo se encontraron correspondencias con el bus introducido.");
             }
 
             catch (Exception e)
             {
-                Console.WriteLine("Ocurrió un error durante la muestra de datos: " + e.Message);
+                Console.Clear();
+                Console.WriteLine("\n Ocurrió un error durante la muestra de datos:\n" + e.Message);
             }
             
+        }
+
+        // Método para obtener el nombre de todas las paradas dentro de una lista.
+        // Lo hacemos así por eficiencia a la hora de acceder a un recurso de internet.
+        private void obtenerNombresParadas(List<string> paradas)
+        {
+            try
+            {
+                XDocument document = XDocument.Load(pathBusInfo);
+                XElement root = document.Root;
+
+                // Guarda toda la información de las paradas
+                IEnumerable<XElement> paradasInfo = root.Element("paradas").Elements(openDataNameSpace + "parada");
+
+                //Se realiza una consulta buscando todas las paradas que tenemos.
+                // Obtenemos todos los nombres
+                var query = from XElement element in paradasInfo
+                            where paradas.Contains(element.Element(openDataNameSpace + "idparada").Value.ToString())
+                            select new { ID = element.Element(openDataNameSpace + "idparada").Value, NOMBRE = element.Element(openDataNameSpace + "descripcion").Value };
+
+                // Si pudimos obtener la información, añadimos los dos campos al diccionario
+                if (paradasInfo.Count() > 0)
+                {
+                    foreach(var parada in query)
+                    {
+                        // Lo añadimos al diccionario
+                        nombresParadas.Add(parada.ID, parada.NOMBRE);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // No hacemos nada
+            }
+        }
+
+        private void cabeceraInfo(string busNumber)
+        {
+            // Mostramos un pequeño bus
+            Console.Clear();
+
+            // Lo convertimos a número para mostrarlo con 2 caracteres
+            int numeroBus = Convert.ToInt32(busNumber);
+
+            Console.WriteLine("\n        _____________\n" +
+                                "      _/_|[][][][][] | - -\n" +
+                                "     (     Bus " + numeroBus.ToString("00") + "    | - -\n" +
+                                "     =--OO-------OO--=\n");
+            Console.WriteLine(" Tiempo para las llegadas del bus:\n");
+        }
+
+        // Método que devuelve los números de los buses junto con su descripción
+        public string obtenerNumerosBuses()
+        {
+            string buses = "";
+
+            try
+            {
+                XDocument document = XDocument.Load(pathBusInfo);
+                XElement root = document.Root;
+
+                // Guarda toda la información de las líneas. El namespace es el mismo
+                IEnumerable<XElement> busInfo = root.Element("lineas").Elements(openDataNameSpace + "linea");
+
+                //Si pudimos obtener la información, obtenemos el número y nombre de la línea
+                if (busInfo.Count() > 0)
+                {
+                    foreach (XElement e in busInfo)
+                    {
+                        string numLinea = e.Element(openDataNameSpace + "idlinea").Value;
+                        string nombre = e.Element(openDataNameSpace + "descripcion").Value;
+
+                        // Formateamos la salida
+                        buses += String.Format(" {0,-5} {1}\n", numLinea, nombre);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("\n\nNo se pueden obtener los datos de los autobuses.");
+                Console.WriteLine("Pulse una tecla para salir.");
+                Console.ReadLine();
+
+                // Cerramos la aplicación
+                Environment.Exit(0);
+            }
+
+            return buses;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,17 +12,89 @@ namespace Prueba_XMLOpenData
      * 21-01-2016
      * Andrés Camín Fernández
      */
+    /**Añadida la interfaz, splash screen, los nombres de las paradas, nombre del bus,
+    * lista de buses y otras mejoras de aspecto.
+    * 06-02-2016
+    * Mirko Fañez Kertelj
+    * www.mirkoo.es
+    */
     class Program
     {
+        static Buses bus;
+
+        static string buses;
+
         static void Main(string[] args)
         {
-            Buses bus = new Buses();
+            bus = new Buses();
 
-            Console.WriteLine("Introduce la linea de bus a consultar:");
+            // Splash Screen con mensaje de cargando, para la espera de obtener la lista de buses
+            muestraSplashScreen();
+
+            // Obtenemos la lista de los buses disponibles
+            buses = bus.obtenerNumerosBuses();
+
+            // Una vez que ya lo tenemos, quitamos la línea de cargando y pedimos una pulsación
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write("  Pulsa una tecla para empezar.");
+            Console.ReadLine();
+
+            muestraHUD();
+        }
+
+        // Método para mostrar una splash screen, mientras cargan los datos de los buses.
+        private static void muestraSplashScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\n"+
+                "   .-------------------------------------------------------------.\n" +
+                 "  '------..-------------..----------..----------..----------..--.|\n" +
+                 "  |       \\\\            ||          ||          ||          ||  ||\n" +
+                 "  |        \\\\           ||          ||          ||          ||  ||\n" +
+                 "  |    ..   ||  _    _  ||    _   _ || _    _   ||    _    _||  ||\n" +
+                 "  |    ||   || //   //  ||   //  // ||//   //   ||   //   //|| /||\n" +
+                 "  |_.------\"''----------''----------''----------''----------''--'|\n" +
+                 "  .______________________________________________________________|\n" +
+                 "   |)|      |       |       |       |    |         |      ||==|  |\n" +
+                 "   | |      | _ - _ |       |       |    |  .-.    |      ||==| C|\n" +
+                 "   | |  __  |.'.-.' |   _   |   _   |    |.'.-.'.  |  __  | \"__=='\n" +
+                 "   '---------'|( )|'----------------------'|( )|'----------\"\"\n" +
+                 "               '-'                          '-'\n" +
+                 "  --------------------- GIJÓN BUS OPEN DATA ----------------------\n" +
+                 "     Han colaborado en este proyecto:\n" +
+                 "  \tAndrés Camín\n" +
+                 "  \tMirko Fañez\n" + "\n");
+
+            Console.Write("\n  Cargando...");
+        }
+
+        // Método para mostrar la interfaz de usuario
+        private static void muestraHUD()
+        {
+            Console.Clear();
+
+            // Mostramos la lista de los buses
+            if (!buses.Equals(""))
+            {
+                Console.WriteLine(String.Format("\n {0,-5} {1}", "BUS", "DESCRIPCIÓN"));
+                Console.WriteLine("------------------------------------------------------");
+
+                Console.WriteLine(buses);
+            }
+
+            // Preguntamos la línea y hacemos la consulta
+            Console.Write("Introduzca la línea de bus a consultar: ");
             bus.query(Console.ReadLine());
 
-            Console.Write("Pulse una tecla para continuar...");
-            Console.ReadKey();
+            // Preguntamos si quiere consultar otro bus
+            Console.Write("\nPulse ESC para salir, o cualquier otra tecla para consultar otra línea.");
+            ConsoleKeyInfo cki = Console.ReadKey();
+
+            if (cki.Key != ConsoleKey.Escape)
+            {
+                // Si no presiona ESC, preguntamos por otro bus
+                muestraHUD();
+            }
         }
     }
 }
